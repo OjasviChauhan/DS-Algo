8c0522645d4ea301771342ea3ad475d690d277d2
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-08-20 22:57:12 +0530

    Update Subset Sum Problem.cpp

--- Subset Sum Problem.cpp
+++ Subset Sum Problem.cpp
@@ -12,6 +12,29 @@ int mod1=1000000007;
 int mod2=67280421310721;
 int mod3=998244353;
 int INF=1e18;
+
+bool subsetSum(int arr[],int sum,int n){
+    bool dp[n+1][sum+1];
+
+    for(int j=0;j<=sum;j++){
+    	dp[0][j] = false;
+    }
+	for(int i=0;i<=n;i++){
+        dp[i][0] = true;
+    }
+    	
+    for(int i=1;i<=n;i++){
+    	for(int j=1;j<=sum;j++){
+    		if(arr[i-1]<=j){
+    			dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+    		}
+    		else{
+    			dp[i][j] = dp[i-1][j];
+    		}
+    	}
+    }
+    return dp[n][sum];
+}
  
 int32_t main(){
 ios_base::sync_with_stdio(false);
@@ -29,27 +52,7 @@ ios_base::sync_with_stdio(false);
     	}
     	int sum;
     	cin>>sum;
-        
-    	bool dp[n+1][sum+1];
-
-    	for(int j=0;j<=sum;j++){
-    		dp[0][j] = false;
-    	}
-    	for(int i=0;i<=n;i++){
-    	    dp[i][0] = true;
-    	}
-    	
-        for(int i=1;i<=n;i++){
-    		for(int j=1;j<=sum;j++){
-    			if(arr[i-1]<=j){
-    				dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
-    			}
-    			else{
-    				dp[i][j] = dp[i-1][j];
-    			}
-    		}
-    	}
-    	cout<<dp[n][sum]<<endl;
+        cout<<subsetSum(arr,sum,n)<<endl;    
     }
 	return 0;
 }
