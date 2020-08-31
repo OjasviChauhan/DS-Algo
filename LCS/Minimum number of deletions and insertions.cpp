757dd569b9779a43cd3597f2188c5cef975f7f7a
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-08-31 19:45:35 +0530

    Update Minimum number of deletions and insertions.cpp

--- LCS/Minimum number of deletions and insertions.cpp
+++ LCS/Minimum number of deletions and insertions.cpp
@@ -1,17 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define fl(i, a, b) for (int i = (a); i < (b); i++)
-#define flr(i, a, b) for(int i = (a); i >=(b); i--)
-#define itr(i, A) for(auto i=A.begin(); i != A.end(); ++i)
-#define pb push_back
-#define mp make_pair
-#define endl '\n'
-#define int long long
-typedef vector<int> VI;
-int mod1=1000000007;
-int mod2=67280421310721;
-int mod3=998244353;
-int INF=1e18;
  
 int LCS(string x,string y,int m,int n){
     int dp[m+1][n+1];
@@ -49,8 +37,8 @@ ios_base::sync_with_stdio(false);
         string x,y;
         cin>>x>>y;
         int lcs = LCS(x,y,m,n);
-        int deletion = m-lcs;
-        int insertion = n-lcs;
+        int deletion = m-lcs;		// This is main change.
+        int insertion = n-lcs;		// This is main change.
         cout<<insertion+deletion<<endl;
     }
 	return 0;
