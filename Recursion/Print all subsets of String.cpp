2d1eb2a4fc4da0bc2d40d4e5bf53017b491e48c7
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-09-19 18:40:24 +0530

    Update Print all subsets of String.cpp

--- Recursion/Print all subsets of String.cpp
+++ Recursion/Print all subsets of String.cpp
@@ -9,9 +9,9 @@ void solve(string ip,string op){
     string op1 = op;
     string op2 = op;
     op2 += ip[0];
-    ip.erase(ip.begin()+0);
-    solve(ip,op1);
-    solve(ip,op2);
+
+    solve(ip.substr(1,ip.length()),op1);
+    solve(ip.substr(1,ip.length()),op2);
 }
 
 int main(){
