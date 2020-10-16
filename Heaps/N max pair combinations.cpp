5c464aed85736fc81bd9df1d4dab902878e34a72
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-10-16 12:59:10 +0530

    Update N max pair combinations.cpp

--- Heaps/N max pair combinations.cpp
+++ Heaps/N max pair combinations.cpp
@@ -1,3 +1,23 @@
+/*
+Given two arrays A & B of size N each.
+Find the maximum N elements from the sum combinations (Ai + Bj) formed from elements in array A and B.
+
+For example if A = [1,2], B = [3,4], then possible pair sums can be 1+3 = 4 , 1+4=5 , 2+3=5 , 2+4=6
+and maximum 2 elements are 6, 5
+
+Example:
+
+N = 4
+a[]={1,4,2,3}
+b[]={2,5,1,6}
+
+Maximum 4 elements of combinations sum are
+10   (4+6), 
+9    (3+6),
+9    (4+5),
+8    (2+6)
+*/
+
 typedef pair<int, pair<int,int>> pi;
 vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
     priority_queue<pi> pq;
@@ -32,3 +52,5 @@ vector<int> Solution::solve(vector<int> &A, vector<int> &B) {
 
 // Time: O(NlogN)
 // Space: O(N)
+
+// https://www.interviewbit.com/problems/n-max-pair-combinations/
