5b8dfbfec81d18616f4f3d23b77898bf02eb74b6
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-10-31 23:38:45 +0530

    Create bfs.cpp

---
+++ Binary Trees/bfs.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:      
+        int data;
+        node* left;
+        node* right;
+          
+        node(int d){
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+node* buildTree(){
+    int d;
+    cin>>d;
+    
+    if(d==-1){
+        return NULL;
+    }
+    
+    node* root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void bfs(node* root){
+    queue<node*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        node* d = q.front();
+        cout<<d->data<<" ";
+        
+        if(d->left){
+            q.push(d->left);
+        }
+        if(d->right){
+            q.push(d->right);
+        }
+        q.pop();
+    }
+}
+
+int main() {
+    node* root = buildTree();
+    cout<<"Level Order: ";
+    bfs(root);
+}
