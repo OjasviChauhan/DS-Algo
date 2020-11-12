d6f6bfa2565f893c91a9a10d9704817cd05aec93
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-11-12 12:30:50 +0530

    Create Single Number.cpp

---
+++ Bit Manipulation/Single Number.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/single-number/
+
+/*
+
+1. If we take XOR of zero and some bit, it will return that bit
+    a)   a ⊕ 0 = a
+    
+2. If we take XOR of two same bits, it will return 0
+    b)   a ⊕ a = 0
+    
+3. a⊕b⊕a = (a⊕a)⊕b = 0⊕b = b
+So we can XOR all bits together to find the unique number.
+
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans=0;
+        for(int x:nums){
+            ans ^= x;
+        }
+        return ans;
+    }
+};
