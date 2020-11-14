a6b49ae9000743ce0a839f7452373cc9f688b95e
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-11-14 14:19:33 +0530

    Update bfs Zigzag (Spiral Order Traversal).cpp

--- Binary Trees/bfs Zigzag (Spiral Order Traversal).cpp
+++ Binary Trees/bfs Zigzag (Spiral Order Traversal).cpp
@@ -1,68 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-class node{
-    public:      
-        int data;
-        node* left;
-        node* right;
-          
-        node(int d){
-            data = d;
-            left = NULL;
-            right = NULL;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void zigZag(TreeNode* root, vector<vector<int>> &v){
+        if(root==NULL){
+            return;
         }
+        stack<TreeNode*> s1;
+        stack<TreeNode*> s2;
+        s1.push(root);
+        
+        while(!s1.empty() || !s2.empty()){
+            vector<int> t;
+            while(!s1.empty()){
+                TreeNode* f = s1.top();
+                if(f->left) s2.push(f->left);
+                if(f->right) s2.push(f->right);
+                s1.pop();
+                t.push_back(f->val);
+            }
+            v.push_back(t);
+            t.clear();
+            
+            while(!s2.empty()){
+                TreeNode* f = s2.top();
+                if(f->right) s1.push(f->right);
+                if(f->left) s1.push(f->left);
+                s2.pop();
+                t.push_back(f->val);
+            }
+            if(!t.empty()) v.push_back(t);
+        }
+    }
+    
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> v;
+        zigZag(root,v);
+        return v;
+    }
 };
-
-node* buildTree(){
-    int d;
-    cin>>d;
-    
-    if(d==-1){
-        return NULL;
-    }
-    
-    node* root = new node(d);
-    root->left = buildTree();
-    root->right = buildTree();
-    return root;
-}
-
-void bfs(node* root){
-    if (root == NULL) 
-        return;
-
-    stack<node*> s1;
-    stack<node*> s2;
-
-    s1.push(root);
-
-    while(!s1.empty() || !s2.empty()){
-        while(!s1.empty()){
-            node* d = s1.top();
-            cout<<d->data<<" ";
-            s1.pop();
-
-            if(d->left)
-                s2.push(d->left);
-            if(d->right)
-                s2.push(d->right);
-        }
-        while(!s2.empty()){
-            node* d = s2.top();
-            cout<<d->data<<" ";
-            s2.pop();
-
-            if(d->right)
-                s1.push(d->right);
-            if(d->left)
-                s1.push(d->left);
-        }
-    }
-}
-
-int main(){
-    node* root = buildTree();
-    bfs(root);
-    return 0;
-}
