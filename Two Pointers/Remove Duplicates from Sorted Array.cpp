0da2cb1549bb66952efafe1a639f2768f2aa521c
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-11-19 19:00:19 +0530

    Delete Remove Duplicates from Sorted Array.cpp

--- Two Pointers/Remove Duplicates from Sorted Array.cpp
+++
@@ -1,8 +0,0 @@
-int Solution::removeDuplicates(vector<int> &vec) {
-    unordered_set<int> s;
-    for (int i : vec)
-        s.insert(i);
-    vec.assign( s.begin(), s.end() );
-    sort( vec.begin(), vec.end() );
-    return vec.size();
-}
