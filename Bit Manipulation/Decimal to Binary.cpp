b7e99d86c3c25a86d3a3720f6bc477485a65f91a
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-11-19 11:40:01 +0530

    Create Decimal to Binary.cpp

---
+++ Bit Manipulation/Decimal to Binary.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int decimalToBinary(int n){
+    int ans=0;
+    int p=1;
+
+    while(n>0){
+        int lastBit = (n&1);
+        ans += p * lastBit;
+        p = p*10;
+        n = n>>1;
+    }
+    return ans;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<decimalToBinary(n);
+}
