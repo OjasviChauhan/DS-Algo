9ba2182d1e58d7c2b21919724bbe8a40ed9ec645
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-11-29 17:54:19 +0530

    Create Subarray Sums Divisible by K.cpp

---
+++ Mathematics/Maths-I Pigeon Hole Principle/Subarray Sums Divisible by K.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/subarray-sums-divisible-by-k/
+// using pigeon hole principle
+// Time: O(N)
+// Space: O(K)
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& A, int K) {
+        int sum=0;
+        int freq[K];
+        memset(freq,0,sizeof(freq));
+        freq[0] = 1;
+        
+        for(int i=0;i<A.size();i++){
+            sum += A[i];
+            sum %= K;
+            sum = (sum+K)%K;
+            freq[sum]++;
+        }
+        int ans=0;
+        for(int i=0;i<K;i++){
+            int n = freq[i];
+            if(n>=2){
+                ans += n*(n-1)/2; 
+            } 
+        }
+        return ans;
+    }
+};
