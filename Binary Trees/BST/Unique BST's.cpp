edb81d49404ea132ca926a86a59cb50ecbaf6723
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2020-12-10 19:51:03 +0530

    Update Unique BST's.cpp

--- Binary Trees/BST/Unique BST's.cpp
+++ Binary Trees/BST/Unique BST's.cpp
@@ -14,17 +14,6 @@ using namespace std;
 #define print(v) for(auto x:v){cout<<x<<" ";}cout<<endl;
 int mod=1000000007;
 
-int pow2(int a,int b,int m){
-    a%=m;
-    int res=1;
-    while (b>0){
-        if (b & 1)
-        res=res*a%m;
-        a=a*a%m;
-        b>>=1;
-    }
-    return res;
-}
 
 int ncr(int n, int k){
     int res = 1; 
