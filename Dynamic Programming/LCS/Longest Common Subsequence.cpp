3cc8d5d6f6fdfe392fa108129dcd6b7ebb23b2fd
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-01-07 12:36:14 +0530

    Update Longest Common Subsequence.cpp

--- Dynamic Programming/LCS/Longest Common Subsequence.cpp
+++ Dynamic Programming/LCS/Longest Common Subsequence.cpp
@@ -1,18 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
- 
-int LCS(string x,string y,int m,int n){
+int solve(string a, string b) {
+    int m=a.size();
+    int n=b.size();
+
     int dp[m+1][n+1];
     for(int i=0;i<=m;i++){
         for(int j=0;j<=n;j++){
             if(i==0 || j==0){
                 dp[i][j] = 0;
             }
-        }
-    }
-    for(int i=1;i<=m;i++){
-        for(int j=1;j<=n;j++){        
-            if(x[i-1] == y[j-1]){
+            else if(a[i-1] == b[j-1]){
                 dp[i][j] = 1 + dp[i-1][j-1];
             }
             else{
@@ -21,22 +17,4 @@ int LCS(string x,string y,int m,int n){
         }
     }
     return dp[m][n];
-}
-
-int32_t main(){
-ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-    int t;
-    cin>>t;
-    while(t-- > 0)
-    {
-        int m,n;
-        cin>>m>>n;
-        string x,y;
-        cin>>x>>y;
-        int ans = LCS(x,y,m,n);
-        cout<<ans<<endl;
-    }
-	return 0;
 }
