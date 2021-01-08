4abef8c209090a1fc84ec9374b24b74d498d1c31
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-01-09 00:14:30 +0530

    Update Dijkstra's Algorithm.cpp

--- Graphs/Graph 07 - Shortest Paths/Dijkstra's Algorithm.cpp
+++ Graphs/Graph 07 - Shortest Paths/Dijkstra's Algorithm.cpp
@@ -21,35 +21,46 @@ public:
 
     void dijkstra(T src){
         unordered_map<T,int> dist;
+        
+        // Initialize all distances at infinity.
         for(auto p:l){
             dist[p.first] = INT_MAX;
         }
-        dist[src] = 0;
-
+        dist[src] = 0;   // except source
+        
+        // making a set to store the data(distance,name) of nodes.
         set<pair<int,T>> s;
         s.insert({0,src});
 
         while(!s.empty()){
+            // find the node at the minimum distance from src which 
+            // will be at the front of the set.
             auto p = *s.begin();
             int nodeDist = p.first;
             T node = p.second;
 
             s.erase(s.begin());
             
+            // Iterate over the neighbours of the current node
             for(auto nbr:l[node]){
                 if(nodeDist + nbr.second < dist[nbr.first]){
-                    T dest = nbr.first; // the destination node on which we are going
+                    // for updation, we have to erase the old data of destination node
+                    // from the set and insert the new data for that node.
+                    
+                    T dest = nbr.first;     // the destination node on which we are going
                     auto f = s.find({dist[dest], dest});
                     if(f!=s.end()){
                         s.erase(f);
                     }
-
+                    
+                    // Insert the new data
                     dist[dest] = nodeDist + nbr.second;
                     s.insert({dist[dest], dest});
                 }
             }
         }
-
+        
+        // printing all distances
         for(auto d:dist){
             cout<<d.first<<" is located at distance: "<<d.second<<endl;
         }
