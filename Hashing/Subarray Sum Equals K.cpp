678971c034464043e70ca0faca7235213da3f746
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-01-17 00:42:03 +0530

    Create Subarray Sum Equals K.cpp

---
+++ Hashing/Subarray Sum Equals K.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/subarray-sum-equals-k/
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        // Sum => Frequency
+        unordered_map<int,int> mp;
+        int count = 0;
+        int sum = 0;
+        
+        // Sum of 0 shows up once.
+        mp[sum] = 1;
+        for(int num : nums){
+            sum += num;
+    
+            if(mp.find(sum-k) != mp.end())
+                count += mp[sum-k];
+            
+            mp[sum]++;
+        }
+        
+        return count;
+    }
+};
