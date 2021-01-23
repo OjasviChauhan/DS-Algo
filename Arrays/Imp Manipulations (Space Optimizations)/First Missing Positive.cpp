614f6fdae3697d808970c0037ae2403e47024824
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-01-23 14:00:25 +0530

    Update First Missing Positive.cpp

--- Arrays/Imp Manipulations (Space Optimizations)/First Missing Positive.cpp
+++ Arrays/Imp Manipulations (Space Optimizations)/First Missing Positive.cpp
@@ -1,3 +1,22 @@
+/*
+Problem : https://leetcode.com/problems/first-missing-positive/
+
+STEP 1: Start traversing the given array.
+
+STEP 2: If number is 0 or negative or greater than array size, just ignore it and move forward.
+
+STEP 3: Otherwise, swap nums[i] with that element in array, whose index is nums[i].
+By doing this, we are actually mapping the element of array to its index i.e. nums[i] --> i without using any extra space.
+
+STEP 4: Before doing swaping, Also check that the index from which we have to swap is already not mapped.
+
+STEP 5: After first iteration, start second iteration of traversing, and return the first element which is not mapped with its index.
+
+NOTE: As it might be 0-based indexing, thats why adjustment of indices is important accordingly.
+
+*/
+
+
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
