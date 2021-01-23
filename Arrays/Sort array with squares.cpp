6cf584851cfcf38029348f145865dd9c1c91bb43
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-01-23 16:49:49 +0530

    Update Sort array with squares.cpp

--- Arrays/Sort array with squares.cpp
+++ Arrays/Sort array with squares.cpp
@@ -1,28 +1,41 @@
-// https://www.interviewbit.com/problems/sort-array-with-squares/
+// https://leetcode.com/problems/squares-of-a-sorted-array/
 
-vector<int> Solution::solve(vector<int> &A){
-    int i=0;
-    while(A[i]<0){i++;}
-    int j = i-1;
+// time: O(n)
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        vector<int> ans;
+        if(A.size()==1){
+            ans.push_back(A[0]*A[0]);
+            return ans;
+        }
+            
+        int i=0;
+        while(A[i]<0){
+            i++;
+            if(i==A.size()) break;
+        }
+        int j = i-1;
     
-    vector<int> ans;
-    while(j>=0 and i<A.size()){
-        if(A[i]*A[i] < A[j]*A[j]){
+        while(j>=0 and i<A.size()){
+            if(A[i]*A[i] < A[j]*A[j]){
+                ans.push_back(A[i]*A[i]);
+                i++;
+            }
+            else{
+                ans.push_back(A[j]*A[j]);
+                j--;
+            }
+        }
+        while(i < A.size()){
             ans.push_back(A[i]*A[i]);
             i++;
         }
-        else{
+        while(j >= 0){
             ans.push_back(A[j]*A[j]);
             j--;
         }
+        return ans;
     }
-    while(i < A.size()){
-        ans.push_back(A[i]*A[i]);
-        i++;
-    }
-    while(j >= 0){
-        ans.push_back(A[j]*A[j]);
-        j--;
-    }
-    return ans;
-}
+};
