7186128f1aa0d54f2f6c13fe707bac1ef08551f9
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-01-27 22:58:25 +0530

    Create Shortest Unsorted Continuous Subarray.cpp

---
+++ Arrays/Shortest Unsorted Continuous Subarray.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& A) {
+        int n = A.size();
+        int i=0, j=n-1;
+        while(i<n-1 and A[i] <= A[i+1]){
+            i++;
+        }
+        while(j>0 and A[j] >= A[j-1]){
+            j--;
+        }
+        if(i == n-1) return 0;
+        
+        int mn = A[i+1], mx = A[i+1];
+        for(int k=i; k<=j; k++){
+		    mx = max(mx, A[k]);
+		    mn = min(mn, A[k]);
+	    }
+        
+        int l = 0, r = n-1;
+	    while(A[l] <= mn and l <= i){
+	   	    l++;
+	    }
+	    while(A[r] >= mx and r >= j){
+		    r--;
+	    }
+        return r-l+1;
+    }
+};
