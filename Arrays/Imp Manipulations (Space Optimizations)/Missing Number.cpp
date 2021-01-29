676b51c1628c05e58b82f92f4396328da55e40f1
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-01-29 20:27:09 +0530

    Create Missing Number.cpp

---
+++ Arrays/Imp Manipulations (Space Optimizations)/Missing Number.cpp
@@ -0,0 +1,22 @@
+
+// https://leetcode.com/problems/missing-number/
+
+/*
+The basic idea is to use XOR operation. We all know that a^b^b =a, which means two xor operations 
+with the same number will eliminate the number and reveal the original number.
+In this solution, I apply XOR operation to both the index and value of the array. 
+In a complete array with no missing numbers, the index and value should be perfectly corresponding( nums[index] = index), 
+so in a missing array, what left finally is the missing number.
+*/
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int res = n;
+        for(int i=0;i<n;i++){
+            res = res ^ i ^ nums[i];
+        }
+        return res;
+    }
+};
