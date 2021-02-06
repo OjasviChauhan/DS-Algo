f570a970936ecb494cc6fd6925d5e78e0f4054d4
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-02-06 23:45:39 +0530

    Update Maximum Area Histogram.cpp

--- Stack/Maximum Area Histogram.cpp
+++ Stack/Maximum Area Histogram.cpp
@@ -1,27 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define endl '\n'
-#define int long long
-
-vector<int> nextSmallerRight(int A[],int n){
+vector<int> nextSmallerRight(vector<int> A,int n){
     vector<int> v;
     stack<pair<int,int>> s;
     
     for(int i=n-1;i>=0;i--){
         if(s.empty()){
-            v.push_back(0);
+            v.push_back(n);
         }
-        else if(!s.empty() && s.top().first<A[i]){
-            v.push_back(0);
+        else if(!s.empty() && A[i]>s.top().first){
+            v.push_back(s.top().second);
         }
-        else if(!s.empty() && s.top().first>=A[i]){
+        else if(!s.empty() && A[i]<=s.top().first){
             while(!s.empty() && s.top().first>=A[i]){
                 s.pop();
             }
             if(s.empty())
-                v.push_back(n-i-1);
+                v.push_back(n);
             else
-                v.push_back(s.top().second-i-1);
+                v.push_back(s.top().second);
         }
         s.push({A[i],i});
     }
@@ -29,56 +24,42 @@ vector<int> nextSmallerRight(int A[],int n){
     return v;
 }
 
-vector<int> nextSmallerLeft(int A[],int n){
+vector<int> nextSmallerLeft(vector<int> A,int n){
     vector<int> v;
     stack<pair<int,int>> s;
     
     for(int i=0;i<n;i++){
         if(s.empty()){
-            v.push_back(0);
+            v.push_back(-1);
         }
-        else if(!s.empty() && s.top().first<A[i]){
-            v.push_back(0);
+        else if(!s.empty() && A[i]>s.top().first){
+            v.push_back(s.top().second);
         }
-        else if(!s.empty() && s.top().first>=A[i]){
+        else if(!s.empty() && A[i]<=s.top().first){
             while(!s.empty() && s.top().first>=A[i]){
                 s.pop();
             }
             if(s.empty())
-                v.push_back(i);
+                v.push_back(-1);
             else
-                v.push_back(i-s.top().second-1);
+                v.push_back(s.top().second);
         }
         s.push({A[i],i});
     }
     return v;
 }
 
-int32_t main(){
-ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-    int t;
-    cin>>t;
-    while(t-- > 0)
-    {
-        int n;
-        cin>>n;
-        int A[n];
-        for(int i=0;i<n;i++){
-            cin>>A[i];
-        }
-        vector<int> r,l;
-        l = nextSmallerLeft(A,n);
-        r = nextSmallerRight(A,n);
+int Solution::largestRectangleArea(vector<int> &A) {
+    vector<int> r,l;
+    int n = A.size();
+    l = nextSmallerLeft(A,n);
+    r = nextSmallerRight(A,n);
         
-        int maxArea=0,area=0;
-        for(int i=0;i<n;i++){
-            area = A[i]*(l[i]+r[i]+1);
-            if(area>maxArea)
-                maxArea=area;
-        }
-        cout<<maxArea<<endl;
+    int maxArea=0,area=0;
+    for(int i=0;i<n;i++){
+        area = A[i]*(r[i]-l[i]-1);
+        if(area>maxArea)
+            maxArea=area;
     }
-	return 0;
+    return maxArea;
 }
