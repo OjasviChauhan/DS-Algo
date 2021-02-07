c81167cfab92f809843c641e6ac103bd72bcecbe
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-02-07 23:43:19 +0530

    Update Median of Two Sorted Arrays.cpp

--- Binary Search/Median of Two Sorted Arrays.cpp
+++ Binary Search/Median of Two Sorted Arrays.cpp
@@ -19,10 +19,10 @@ public:
             int cut1 = lo + (hi-lo)/2;
             int cut2 = (n1+n2)/2 - cut1;
             
-            int l1 = cut1==0 ? INT_MIN : nums1[cut1-1];
-            int l2 = cut2==0 ? INT_MIN : nums2[cut2-1];
-            int r1 = cut1==n1 ? INT_MAX : nums1[cut1];
-            int r2 = cut2==n2 ? INT_MAX : nums2[cut2];
+            int l1 = cut1==0 ? INT_MIN : nums1[cut1-1];  // max element of first left array
+            int l2 = cut2==0 ? INT_MIN : nums2[cut2-1];  // max element of second left array
+            int r1 = cut1==n1 ? INT_MAX : nums1[cut1];   // min element of first right array
+            int r2 = cut2==n2 ? INT_MAX : nums2[cut2];   // min element of second right array
             
             if(l1 > r2)
                 hi = cut1-1;
