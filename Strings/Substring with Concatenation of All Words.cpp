7eab18eb509da1db738aa9f395d90f80918dd03f
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-02-09 19:47:42 +0530

    Update Substring with Concatenation of All Words.cpp

--- Strings/Substring with Concatenation of All Words.cpp
+++ Strings/Substring with Concatenation of All Words.cpp
@@ -1,29 +1,60 @@
 // https://leetcode.com/problems/substring-with-concatenation-of-all-words/
 
+// travel all the words combinations to maintain a window
+// there are wl(word len) times travel
+// each time, n/wl words, mostly 2 times travel for each word
+// one left side of the window, the other right side of the window
+// so, time complexity O(wl * 2 * N/wl) = O(2N)
+
 class Solution {
 public:
-    vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string,int> m1;
-        for(int i=0;i<words.size();i++){
-            m1[words[i]]++;
-        }
-        
+    vector<int> findSubstring(string S, vector<string> &L) {
         vector<int> ans;
-        int ws = words[0].size();
-        int l1 = ws * words.size();
+        int n = S.size(), cnt = L.size();
+        if (n <= 0 || cnt <= 0) return ans;
         
-        for(int i=0; i+l1 <= s.size(); i++){
-            string temp = s.substr(i, l1);
-            unordered_map<string,int> m2;
+        // init word occurence
+        unordered_map<string, int> dict;
+        for (int i = 0; i < cnt; ++i) dict[L[i]]++;
+        
+        // travel all sub string combinations
+        int wl = L[0].size();
+        for (int i = 0; i < wl; ++i){
+            int left = i, count = 0;
+            unordered_map<string, int> tdict;
             
-            int k=0;
-            while(k+ws <= temp.size()){
-                m2[temp.substr(k,ws)]++;
-                k += ws;
+            for (int j = i; j <= n - wl; j += wl) {
+                string str = S.substr(j, wl);
+                // a valid word, accumulate results
+                if (dict.count(str)) {
+                    tdict[str]++;
+                    if (tdict[str] <= dict[str]) 
+                        count++;
+                    else {
+                        // a more word, advance the window left side possiablly
+                        while (tdict[str] > dict[str]) {
+                            string str1 = S.substr(left, wl);
+                            tdict[str1]--;
+                            if (tdict[str1] < dict[str1]) count--;
+                            left += wl;
+                        }
+                    }
+                    // come to a result
+                    if (count == cnt) {
+                        ans.push_back(left);
+                        // advance one word
+                        tdict[S.substr(left, wl)]--;
+                        count--;
+                        left += wl;
+                    }
+                }
+                // not a valid word, reset all vars
+                else {
+                    tdict.clear();
+                    count = 0;
+                    left = j + wl;
+                }
             }
-            
-            if(m1==m2)
-                ans.push_back(i);
         }
         return ans;
     }
