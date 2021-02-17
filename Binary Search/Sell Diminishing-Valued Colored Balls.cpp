bd823f123636272867cd0c157d8d8287ab6a5bea
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-02-17 13:25:37 +0530

    Update Sell Diminishing-Valued Colored Balls.cpp

--- Binary Search/Sell Diminishing-Valued Colored Balls.cpp
+++ Binary Search/Sell Diminishing-Valued Colored Balls.cpp
@@ -1,36 +1,42 @@
 // https://leetcode.com/problems/sell-diminishing-valued-colored-balls/
 
-#define mod 1000000007
 class Solution {
-public:
-    map<int, int, greater<>> mp;
-
-    bool isValid(int M, int k){
-        for(auto &[i, cnt] : mp){
-            if(i <= M) break;
-            k -= (long)cnt * (i - M);
-            if(k <= 0) return true; 
+public: 
+    vector<int> v;
+    
+    bool isValid(long val , long order){
+        for(auto x : v){
+            if(x < val)break;
+            order -= (x-val+1);
         }
-        return k <= 0;
+        if(order > 0)return 0;
+        else return 1;
     }
     
-    int maxProfit(vector<int> &A, int orders) {
-        long ans = 0, L = 0, R = *max_element(begin(A), end(A));
-        for (int i : A) mp[i]++;
+    int maxProfit(vector<int>& nums, int order) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        v = nums;
+        long s=0, e = v[0], ans, p = 1e9+7;
         
-        while(L <= R){
-            long M = (L + R) / 2;
-            if (isValid(M, orders)) L = M + 1;
-            else R = M - 1;
+        while(s <= e){
+            long mid = (s + e)/2;
+            if(isValid(mid,order)){
+                ans=mid;
+                s=mid+1;
+            }
+            else e = mid-1;
+        }
+
+        long res=0;
+        for(auto x : v){
+            if(x <= ans) break;
+            order -= (x-ans);
+            res += ((long)x*(x+1)/2 - ans*(ans+1)/2) % p;
+            res %= p;
         }
         
-        for(auto &[i, cnt] : mp){
-            if(i <= L) break;
-            orders -= cnt * (i - L);
-            ans = (ans + (i+L+1)*(i-L)/2 % mod * cnt % mod) % mod;
-        }
-        if(orders > 0) ans = (ans + L*orders % mod) % mod;
-        
-        return ans;
+        if(order > 0) res += ans*order % p;
+        res %= p;
+        return res;
     }
 };
