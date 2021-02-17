74f986bc5e4dba38625949cd83080ec8e66b6cd6
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-02-17 20:23:26 +0530

    Update Power Function(mod).cpp

--- Mathematics/Maths-III Solving Linear Recurrences/Power Function(mod).cpp
+++ Mathematics/Maths-III Solving Linear Recurrences/Power Function(mod).cpp
@@ -1,10 +1,10 @@
 // https://www.interviewbit.com/problems/implement-power-function/
 
 int power(long long x, long long n, int& m) {
- 	if (n == 0)
-		return 1;
+     if (n == 0)
+        return 1;
 
-	return (n%2 == 0) ? power(x*x % m, n/2, m)%m : x*power(x*x % m, n/2, m)%m;
+    return (n%2 == 0) ? power(x*x % m, n/2, m)%m : x*power(x*x % m, n/2, m)%m;
 }
 
 int Solution::pow(int x, int n, int d) {
@@ -13,7 +13,7 @@ int Solution::pow(int x, int n, int d) {
         
     int res = power(x, n, d);
     if (res < 0)
-        return d + (res % d);
+        return (d + res) % d;
         
     return res;
 }
