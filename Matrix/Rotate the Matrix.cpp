b4efedf57ca517f8208e8ab615e4a8e992fd3cfd
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-02-18 23:57:29 +0530

    Update Rotate the Matrix.cpp

--- Matrix/Rotate the Matrix.cpp
+++ Matrix/Rotate the Matrix.cpp
@@ -6,9 +6,9 @@ void Solution::rotate(vector<vector<int> > &A) {
         
     while(k <= n/2){
         while(r<n-k and d<n-k and l>=k and u>=k){
-            swap(A[k-1][r],A[d][n-k]);
-            swap(A[k-1][r],A[n-k][l]);
-            swap(A[k-1][r],A[u][k-1]);
+            swap(A[k-1][r], A[d][n-k]);
+            swap(A[k-1][r], A[n-k][l]);
+            swap(A[k-1][r], A[u][k-1]);
             r++;
             d++;
             l--;
