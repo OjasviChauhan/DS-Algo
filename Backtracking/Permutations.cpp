d3bd5c10fa933cfa98acf342afc1f86e6df0fbe8
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-02-23 12:51:01 +0530

    Update Permutations.cpp

--- Backtracking/Permutations.cpp
+++ Backtracking/Permutations.cpp
@@ -1,5 +1,38 @@
 // https://leetcode.com/problems/permutations/
 
+class Solution {
+public:
+    vector<vector<int>> res;
+    void solve(int n,vector<int> &nums,vector<int> &ds,bool used[]){
+        if(ds.size() == n){
+            res.push_back(ds);
+            return;
+        }
+        for(int i=0; i<nums.size(); i++){
+            if(used[i] or i>0 and nums[i]==nums[i-1]) continue;
+            ds.push_back(nums[i]);
+            used[i] = 1;
+            solve(n,nums,ds,used);
+            ds.pop_back();
+            used[i] = 0;
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> ds;
+        int n = nums.size();
+        bool used[n];
+        memset(used,0,sizeof used);
+        sort(nums.begin(),nums.end());
+        
+        solve(n,nums,ds,used);
+        return res;
+    }
+};
+
+
+// Different Solution
+
 void solve(vector<int> &A,int n,vector<vector<int>> &ans,vector<int> &ds){
     if(ds.size()==n){
         ans.push_back(ds);
