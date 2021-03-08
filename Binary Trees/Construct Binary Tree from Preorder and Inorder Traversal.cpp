17ca15117b0300060f66495989368e7fad46ee8f
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-03-08 21:26:48 +0530

    Update and rename Build Binary Tree from preorder & inorder (OPTIMIZED).cpp to Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- Binary Trees/Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Binary Trees/Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -10,17 +12,15 @@
  * };
  */
 class Solution {
-    private:
         int i=0;
         unordered_map<int,int> mp;
     public:
-        TreeNode* solve(vector<int>& preorder, vector<int>& inorder,int s,int e){
+        TreeNode* solve(vector<int>& preorder,int s,int e){
             if(s>e) return NULL;
             TreeNode* root = new TreeNode(preorder[i]);
-            int index = mp[preorder[i]];
-            i++;
-            root->left = solve(preorder,inorder,s,index-1);
-            root->right = solve(preorder,inorder,index+1,e);
+            int index = mp[preorder[i++]];
+            root->left = solve(preorder,s,index-1);
+            root->right = solve(preorder,index+1,e);
             return root;
         }
 
@@ -28,6 +28,6 @@ class Solution {
             for(int i=0;i<inorder.size();i++){
                 mp[inorder[i]] = i;
             }
-            return solve(preorder,inorder,0,inorder.size()-1);
+            return solve(preorder,0,inorder.size()-1);
         }
 };
