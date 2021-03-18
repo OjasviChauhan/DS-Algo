021eb04f45020ce0f8b525a9194bfc3c97274c7c
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-03-18 11:19:06 +0530

    Create Unique Binary Search Trees II.cpp

---
+++ Binary Trees/Tree Construction/Unique Binary Search Trees II.cpp
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/unique-binary-search-trees-ii/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> solve(int s,int e){
+        vector<TreeNode*> ans;
+        if(s > e){
+            ans.push_back(NULL);
+            return ans;
+        }
+        
+        for(int i=s; i<=e; i++){
+            vector<TreeNode*> leftBSTs = solve(s,i-1);
+            vector<TreeNode*> rightBSTs = solve(i+1,e);
+            
+            for(auto l : leftBSTs) {
+                for(auto r : rightBSTs){
+                    TreeNode* newNode = new TreeNode(i, l, r);
+                    ans.push_back(newNode);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    vector<TreeNode*> generateTrees(int n) {    
+        return solve(1,n);
+    }
+};
