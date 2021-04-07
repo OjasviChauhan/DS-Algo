2ac800dbea2f816347100f1402ca8a0fcca5c6d3
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-04-07 21:21:07 +0530

    Update Fast Exponentiation (LogN).cpp

--- Mathematics/Maths-III Solving Linear Recurrences/Fast Exponentiation (LogN).cpp
+++ Mathematics/Maths-III Solving Linear Recurrences/Fast Exponentiation (LogN).cpp
@@ -1,22 +1,45 @@
-// https://leetcode.com/problems/powx-n/
+#include <iostream>
+using namespace std;
 
-class Solution {
-public:
-    double pow(double x, int n){
-        if(n==0) 
-            return 1;
-        if(n == INT_MIN){
-            x = x * x;
-            n = n/2;
-        }
-        if(n<0){
-            n = -n;
-            x = 1/x;
-        }
-        return (n%2 == 0) ? pow(x*x, n/2) : x*pow(x*x, n/2);
+/*
+Binary exponentiation (also known as exponentiation by squaring) 
+is a trick which allows to calculate an using only O(logn) 
+multiplications (instead of O(n) multiplications required 
+by the naive approach).
+*/
+
+long long powRecursive(long long x, long long n) {
+    if (n == 0)
+        return 1;
+    long long res = powRecursive(x, n / 2);
+    if (n % 2 == 0)
+        return res * res;
+    else
+        return res * res * x;
+}
+
+/*
+The second approach accomplishes the same task without recursion. 
+It computes all the powers in a loop, and multiplies the ones with 
+the corresponding set bit in n. Although the complexity of both 
+approaches is identical, this approach will be faster in practice
+since we have the overhead of the recursive calls.
+*/
+
+long long powIterative(long long x, long long n){
+    long long res = 1;
+    while(n > 0){
+        if(n & 1)
+            res = res * x;
+        x = x * x;
+        n >>= 1;
     }
+    return res;
+}
+
+int main() {
+
+    cout << powRecursive(2,5) << endl;
+    cout << powIterative(2,5) << endl;
     
-    double myPow(double x, int n) {
-        return pow(x,n);
-    }
-};
+}
