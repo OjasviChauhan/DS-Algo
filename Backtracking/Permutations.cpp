40e6ab55db94bd8c1f058840dd28b5e134724c31
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-05-16 00:35:50 +0530

    Update Permutations.cpp

--- Backtracking/Permutations.cpp
+++ Backtracking/Permutations.cpp
@@ -3,29 +3,28 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    void solve(int n,vector<int> &nums,vector<int> &ds,bool used[]){
-        if(ds.size() == n){
+    void solve(vector<int> &nums, vector<int> &ds, bool used[]){
+        if(ds.size() == nums.size()){
             res.push_back(ds);
             return;
         }
-        for(int i=0; i<nums.size(); i++){
-            if(used[i] or i>0 and nums[i]==nums[i-1]) continue;
-            ds.push_back(nums[i]);
-            used[i] = 1;
-            solve(n,nums,ds,used);
-            ds.pop_back();
-            used[i] = 0;
+        for(int i=0; i < nums.size(); i++){
+            if(!used[i]){
+                ds.push_back(nums[i]);
+                used[i] = 1;
+                solve(nums,ds,used);
+                ds.pop_back();
+                used[i] = 0;
+            }
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<int> ds;
-        int n = nums.size();
-        bool used[n];
+        bool used[nums.size()];
         memset(used,0,sizeof used);
-        sort(nums.begin(),nums.end());
         
-        solve(n,nums,ds,used);
+        solve(nums,ds,used);
         return res;
     }
 };
