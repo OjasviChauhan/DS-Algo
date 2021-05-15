216372784ff788e18903719be777d32f0defeab9
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-05-15 10:52:10 +0530

    Create Gray Code.cpp

---
+++ Bit Manipulation/Gray Code.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/gray-code/
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> ans(1<<n);
+        for(int i=0; i < (1<<n); i++){
+            ans[i] = i ^ (i >> 1);
+        }
+        return ans;
+    }
+};
