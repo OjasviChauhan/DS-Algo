337151d03d0eea675a74fa2cd79bbd555f63cc66
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-05-18 21:33:39 +0530

    Update Sudoku Solver.cpp

--- Backtracking/Sudoku Solver.cpp
+++ Backtracking/Sudoku Solver.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     bool canPlace(vector<vector<char>>& board,int i,int j,int n,char number){
         // row and col check
-        for(int x=0;x<n;x++){
+        for(int x = 0; x < n; x++){
             if(board[i][x]==number or board[x][j]==number){
                 return false;
             }
@@ -12,11 +12,11 @@ public:
         
         // sub-grid check
         int rn = sqrt(n);
-        int sx = (i/rn)*rn;
-        int sy = (j/rn)*rn;
+        int sx = (i / rn) * rn;
+        int sy = (j / rn) * rn;
         
-        for(int x=sx;x<sx+rn;x++){
-            for(int y=sy;y<sy+rn;y++){
+        for(int x = sx; x < sx + rn; x++){
+            for(int y = sy; y < sy + rn; y++){
                 if(board[x][y] == number){
                     return false;
                 }
@@ -28,23 +28,23 @@ public:
     
     bool solve(vector<vector<char>>& board,int i,int j,int n){
         // Base Cases
-        if(i==n){
+        if(i == n){
             return true;
         }
-        if(j==n){
-            return solve(board,i+1,0,n);
+        if(j == n){
+            return solve(board, i + 1, 0, n);
         }
         if(board[i][j] != '.'){
-            return solve(board,i,j+1,n);
+            return solve(board, i, j + 1, n);
         }
         
         // Main Recursive Case
-        for(int number=1;number<=9;number++){
+        for(int number = 1; number <= 9; number++){
             char c = number + 48;
-            if(canPlace(board,i,j,n,c)){
+            if(canPlace(board, i, j, n, c)){
                 board[i][j] = number + 48;
             
-                if(solve(board,i,j+1,n)){
+                if(solve(board, i, j + 1, n)){
                     return true;
                 }
             }
@@ -56,6 +56,6 @@ public:
     
     void solveSudoku(vector<vector<char>>& board) {
         int n = board.size();
-        solve(board,0,0,n);
+        solve(board, 0, 0, n);
     }
 };
