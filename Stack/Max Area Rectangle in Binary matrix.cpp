a5b2a37f1aebe93329377509be066273bb7ac32e
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-06-02 23:55:28 +0530

    Update Max Area Rectangle in Binary matrix.cpp

--- Stack/Max Area Rectangle in Binary matrix.cpp
+++ Stack/Max Area Rectangle in Binary matrix.cpp
@@ -2,102 +2,107 @@
 using namespace std;
 #define MAX 1000
 
-vector<int> nextSmallerRight(int A[],int n){
-    vector<int> v;
-    stack<pair<int,int>> s;
-    
-    for(int i=n-1;i>=0;i--){
-        if(s.empty()){
-            v.push_back(0);
-        }
-        else if(!s.empty() && s.top().first<A[i]){
-            v.push_back(0);
-        }
-        else if(!s.empty() && s.top().first>=A[i]){
-            while(!s.empty() && s.top().first>=A[i]){
-                s.pop();
-            }
-            if(s.empty())
-                v.push_back(n-i-1);
-            else
-                v.push_back(s.top().second-i-1);
-        }
-        s.push({A[i],i});
-    }
-    reverse(v.begin(),v.end());
-    return v;
-}
-
-vector<int> nextSmallerLeft(int A[],int n){
-    vector<int> v;
-    stack<pair<int,int>> s;
-    
-    for(int i=0;i<n;i++){
-        if(s.empty()){
-            v.push_back(0);
-        }
-        else if(!s.empty() && s.top().first<A[i]){
-            v.push_back(0);
-        }
-        else if(!s.empty() && s.top().first>=A[i]){
-            while(!s.empty() && s.top().first>=A[i]){
-                s.pop();
-            }
-            if(s.empty())
-                v.push_back(i);
-            else
-                v.push_back(i-s.top().second-1);
-        }
-        s.push({A[i],i});
-    }
-    return v;
-}
-
-int MAH(int A[],int n){
-    vector<int> r,l;
-    l = nextSmallerLeft(A,n);
-    r = nextSmallerRight(A,n);
+class Solution{
+  public:
+    vector<int> nextSmallerLeft(int A[], int n){
+        vector<int> res;
+        stack<pair<int,int>> s;
         
-    int Max=0,area=0;
-    for(int i=0;i<n;i++){
-        area = A[i]*(l[i]+r[i]+1);
-        if(area>Max)
-            Max=area;
-    }
-    return Max;
-}
-
-int maxArea(int M[MAX][MAX], int n, int m) {
-    int v[m];
-    for(int j=0;j<m;j++){
-        v[j] = M[0][j];
-    }
-    int mx = MAH(v,m);
-    for(int i=1;i<n;i++){
-        for(int j=0;j<m;j++){
-            if(M[i][j]==0)
-                v[j]=0;
-            else
-                v[j] += M[i][j];
+        for(int i=0;i<n;i++){
+            if(s.empty()){
+                res.push_back(-1);
+            }
+            else if(!s.empty() and A[i] > s.top().first){
+                res.push_back(s.top().second);
+            }
+            else if(!s.empty() and A[i] <= s.top().first){
+                while(!s.empty() and A[i] <= s.top().first){
+                    s.pop();
+                }
+                if(s.empty())
+                    res.push_back(-1);
+                else
+                    res.push_back(s.top().second);
+            }
+            s.push({A[i], i});
         }
-        mx = max(mx,MAH(v,m));
+        return res;
     }
-    return mx;
-}
-
-int main(){
-  int t;
-  cin>>t;
-  int M[MAX][MAX];
-  while(t-->0){
-    int n,m;
-    cin>>n>>m;
     
-    for(int i=0;i<n;i++){
-      for(int j=0;j<m;j++){
-        cin>>M[i][j];
-      }
+    vector<int> nextSmallerRight(int A[], int n){
+        vector<int> res;
+        stack<pair<int,int>> s;
+        
+        for(int i = n - 1; i >= 0; i--){
+            if(s.empty()){
+                res.push_back(n);
+            }
+            else if(A[i] > s.top().first){
+                res.push_back(s.top().second);
+            }
+            else if(A[i] <= s.top().first){
+                while(!s.empty() and A[i] <= s.top().first){
+                    s.pop();
+                }
+                if(s.empty())
+                    res.push_back(n);
+                else
+                    res.push_back(s.top().second);
+            }
+            s.push({A[i], i});
+        }
+        reverse(res.begin(), res.end());
+        return res;
     }
-    cout<<maxArea(M,n,m)<<endl;
-  }
+    
+    int MAH(int A[],int n){
+        vector<int> l = nextSmallerLeft(A, n);
+        vector<int> r = nextSmallerRight(A, n);
+
+        int mx = 0, area = 0;
+        for(int i=0; i < n; i++){
+            int area = A[i]*(r[i]-l[i]-1);
+            mx = max(mx, area);
+        }
+        return mx;
+    }
+  
+    int maxArea(int M[MAX][MAX], int n, int m) {
+        int v[m];
+        for(int j = 0; j < m; j++){
+            v[j] = M[0][j];
+        }
+        int ans = MAH(v,m);
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(M[i][j])
+                    v[j] += M[i][j];
+                else
+                    v[j] = 0;
+            }
+            ans = max(ans, MAH(v,m));
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int T;
+    cin >> T;
+
+    int M[MAX][MAX];
+
+    while (T--) {
+        int n, m;
+        cin >> n >> m;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cin >> M[i][j];
+            }
+        }
+        Solution obj;
+        cout << obj.maxArea(M, n, m) << endl;
+    }
 }
