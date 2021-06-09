c19f9ec23e2c4946d0e8dbd6f75fd19ac5e4d8d5
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-06-09 19:41:19 +0530

    Update Parenthesis Checker.cpp

--- Stack/Parenthesis Checker.cpp
+++ Stack/Parenthesis Checker.cpp
@@ -1,63 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define endl '\n'
-#define int long long
+// https://practice.geeksforgeeks.org/problems/parenthesis-checker2744/1#
 
-void solve(string str){
-    stack<char> s;
-    int n = str.length();
-    for(int i=0;i<str.length();i++){
-        if(str[i]=='{' || str[i]=='[' || str[i]=='('){
-            s.push(str[i]);
-        }
-        
-        if (s.empty()){
-            cout<<"not balanced"<<endl;
-            return;
-        }
-        
-        if(str[i]=='}'){
-            char x = s.top();
-            s.pop();
-            if(x=='[' || x=='('){
-                cout<<"not balanced"<<endl;
-                return;
+class Solution{
+    public:
+    bool ispar(string x){
+        stack<char> s;
+        for(char &c : x){
+            if(c == '[' or c=='{' or c=='(') s.push(c);
+            else if(c == ']'){
+                if(!s.empty() and s.top() == '[') 
+                    s.pop();
+                else return false;
+            }
+            else if(c == '}'){
+                if(!s.empty() and s.top() == '{') 
+                    s.pop();
+                else return false;
+            }
+            else if(c == ')'){
+                if(!s.empty() and s.top() == '(') 
+                    s.pop();
+                else return false;
             }
         }
-        if(str[i]==']'){
-            char x = s.top();
-            s.pop();
-            if(x=='{' || x=='('){
-                cout<<"not balanced"<<endl;
-                return;
-            }
-        }
-        if(str[i]==')'){
-            char x = s.top();
-            s.pop();
-            if(x=='[' || x=='{'){
-                cout<<"not balanced"<<endl;
-                return;
-            }
-        }
+        if(s.size()) return false;
+        return true;
     }
-    if(s.empty())
-        cout<<"balanced"<<endl;
-    else
-        cout<<"not balanced"<<endl;
-}
- 
-int32_t main(){
-ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-    int t;
-    cin>>t;
-    while(t-- > 0)
-    {
-        string s;
-        cin>>s;
-        solve(s);
-    }
-	return 0;
-}
+};
