7671601fdb1f153bd8cd25a9c61b3ee114eb4730
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-06-10 14:33:42 +0530

    Update Majority Element.cpp

--- Arrays/Imp Manipulations (Space Optimizations)/Majority Element.cpp
+++ Arrays/Imp Manipulations (Space Optimizations)/Majority Element.cpp
@@ -27,3 +27,41 @@ public:
         return maj;
     }
 };
+
+
+// if question says that there is no gurantee for presence of majority element.
+// And if not present then you have to return -1. In that case, first we'll have
+// to check for possible candidate for majority element and then confirm it by
+// checking its frequency i.e it should be greater than size/2.
+
+int findCandidate(vector<int> a, int n){
+    int maj = a[0], cnt = 1;
+    for (int i = 1; i < n; i++) {
+        if (maj == a[i]) cnt++;
+        else cnt--;
+        if (cnt == 0) {
+            maj = a[i];
+            cnt = 1;
+        }
+    }
+    return maj;
+}
+    
+bool isMajority(vector<int> a, int n, int cand) {
+    int cnt = 0;
+    for (int i = 0; i < n; i++){
+        if (a[i] == cand)
+            cnt++;
+    }
+    if (cnt > n / 2) return 1;
+    else return 0;
+}
+ 
+int Solution::majorityElement(const vector<int> &a) {
+    int n = a.size();
+    int cand = findCandidate(a, n);
+    if (isMajority(a, n, cand))
+        return cand;
+    else
+        return -1;
+}
