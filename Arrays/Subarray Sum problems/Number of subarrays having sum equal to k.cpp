a9b5a709c4f73aecfa366b4967b46bbb21315f1b
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-06-16 00:13:58 +0530

    Update Number of subarrays having sum equal to k.cpp

--- Arrays/Subarray Sum problems/Number of subarrays having sum equal to k.cpp
+++ Arrays/Subarray Sum problems/Number of subarrays having sum equal to k.cpp
@@ -1,52 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-// Function to find number of subarrays
-// with sum exactly equal to k.
-int findSubarraySum(int arr[], int n, int sum)
-{
-	// STL map to store number of subarrays
-	// starting from index zero having
-	// particular value of sum.
-	unordered_map<int, int> prevSum;
-
-	int res = 0;
-
-	// Sum of elements so far.
-	int currsum = 0;
-
-	for (int i = 0; i < n; i++) {
-
-		// Add current element to sum so far.
-		currsum += arr[i];
-
-		// If currsum is equal to desired sum,
-		// then a new subarray is found. So
-		// increase count of subarrays.
-		if (currsum == sum)
-			res++;
-
-		// currsum exceeds given sum by currsum
-		// - sum. Find number of subarrays having
-		// this sum and exclude those subarrays
-		// from currsum by increasing count by
-		// same amount.
-		if (prevSum.find(currsum - sum) != prevSum.end())
-			res += (prevSum[currsum - sum]);
-
-		// Add currsum value to count of
-		// different values of sum.
-		prevSum[currsum]++;
-	}
-
-	return res;
-}
-
-int main()
-{
-	int arr[] = {1,2,3,0,0,3};
-	int sum = 3;
-	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << findSubarraySum(arr, n, sum);
-	return 0;
-}
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        // Sum => Frequency
+        unordered_map<int,int> mp;
+        int count = 0;
+        int sum = 0;
+        
+        // Sum of 0 shows up once.
+        mp[sum] = 1;
+        for(int num : nums){
+            sum += num;
+    
+            if(mp.find(sum-k) != mp.end())
+                count += mp[sum-k];
+            
+            mp[sum]++;
+        }
+        
+        return count;
+    }
+};
