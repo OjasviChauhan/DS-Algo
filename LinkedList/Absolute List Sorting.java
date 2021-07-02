a780d442fea91088ac57e4d1ca386acae90e211c
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-07-02 12:50:09 +0530

    Create Absolute List Sorting.java

---
+++ LinkedList/Absolute List Sorting.java
@@ -0,0 +1,15 @@
+  // https://practice.geeksforgeeks.org/problems/absolute-list-sorting/1#
+  
+  Node sortList(Node head) {
+		Node t = head;
+        while(t.next != null){
+            if(t.next.data < 0){
+                Node f = t.next;
+                t.next = f.next;
+                f.next = head;
+                head = f;
+            }
+            else t = t.next;
+        }
+        return head;
+  }
