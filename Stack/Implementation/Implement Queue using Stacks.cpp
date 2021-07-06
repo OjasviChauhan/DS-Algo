b4203abc807394403516bfd1c1826dc3eb4fb742
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-07-06 17:03:49 +0530

    Update Implement Queue using Stacks.cpp

--- Stack/Implementation/Implement Queue using Stacks.cpp
+++ Stack/Implementation/Implement Queue using Stacks.cpp
@@ -5,37 +5,32 @@
 
 class MyQueue {
 private:
-    stack<int> *s1;
-    stack<int> *s2;
+    stack<int> s1;
+    stack<int> s2;
 public:
-    MyQueue() {
-        s1 = new stack<int>;
-        s2 = new stack<int>;
-    }
-
     void push(int x) {
-        while(!s1->empty()){
-            s2->push(s1->top());
-            s1->pop();
+        while(!s1.empty()){
+            s2.push(s1.top());
+            s1.pop();
         }
-        s1->push(x);
-        while(!s2->empty()){
-            s1->push(s2->top());
-            s2->pop();
+        s1.push(x);
+        while(!s2.empty()){
+            s1.push(s2.top());
+            s2.pop();
         }
     }
 
     int pop() {
-        int t = s1->top();
-        s1->pop();
+        int t = s1.top();
+        s1.pop();
         return t;
     }
 
     int peek() {
-        return s1->top();
+        return s1.top();
     }
 
     bool empty() {
-        return s1->empty();
+        return s1.empty();
     }
 };
