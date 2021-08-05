6bc54dee46786151b3f959f05fc82d31d47f1be3
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-08-05 10:35:57 +0530

    Update [Sliding Window] Longest Substring Without Repeating Characters.cpp

--- Two Pointers & Sliding Window/[Sliding Window] Longest Substring Without Repeating Characters.cpp
+++ Two Pointers & Sliding Window/[Sliding Window] Longest Substring Without Repeating Characters.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
+// SOLUTION 1
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -22,6 +23,33 @@ public:
             }
         }
         len = max(len, end-start);
+        return len;
+    }
+};
+
+
+
+// SOLUTION 2
+class Solution {
+public:
+    int lengthOfLongestSubstring(string A) {
+        if(A.size() == 0) return 0;
+        int start = 0, end = 0;
+        int len = 0;
+        unordered_map<char,int> f;
+
+        for(end = 0; end < A.size(); end++) {
+            char ch = A[end] - 'a';
+            f[ch]++;
+            if(f[ch] > 1) {
+                len = max(len, end - start);
+                while(f[ch] > 1 and start < end){
+                    f[A[start]-'a']--;
+                    start++;
+                }
+            }
+        }
+        len = max(len, end - start);
         return len;
     }
 };
