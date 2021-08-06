c97ed7533e073e716e0ccf5afda4a5c56f8521ee
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-08-06 12:49:36 +0530

    Update The Painter's Partition Problem.cpp

--- Binary Search/Search on Answer/The Painter's Partition Problem.cpp
+++ Binary Search/Search on Answer/The Painter's Partition Problem.cpp
@@ -1,63 +1,39 @@
-// https://practice.geeksforgeeks.org/problems/the-painters-partition-problem/0#
+// https://www.interviewbit.com/old/problems/painters-partition-problem/
 
-#include <bits/stdc++.h>
-using namespace std;
-#define pb push_back
-#define endl '\n'
-#define int long long
-#define ff first
-#define ss second
-#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
-#define print(v) for(auto x:v){cout<<x<<" ";}cout<<endl;
-int mod=1000000007;
-
-bool isValid(int A[],int mid,int k,int n){
-    int sum = 0, painter = 1;
-    for(int i=0;i<n;i++){
-        sum += A[i];
-        if(sum > mid){
-            painter++;
-            sum = A[i];
+bool check(vector<long> &time, int k, long mid) {
+    long painters = 1, t = 0;
+    for(int i = 0; i < time.size(); i++) {
+        t += time[i];
+        if(t > mid){
+            painters++;
+            t = time[i];
         }
-        if(painter > k)
-            return false;
+        if(painters > k) return false;
     }
     return true;
 }
 
-int32_t main(){
-    fast;
-    
-    int t;
-    cin>>t;
-    while(t--){
-        int k,n;
-        cin>>k>>n;
-        int A[n];
-        for(int i=0;i<n;i++){
-            cin>>A[i];
-        }
-        
-        int mx = INT_MIN, sum = 0;
-        for(int i=0;i<n;i++){
-            sum += A[i];
-            mx = max(mx,A[i]);
-        }
-        
-        int start = mx, end = sum;
-        int ans;
-        
-        while(start<=end){
-            int mid = end - (end-start)/2;
-            if(isValid(A,mid,k,n)){
-                ans = mid;
-                end = mid - 1;
-            }
-            else{
-                start = mid + 1;
-            }
-        }
-        cout<<ans<<endl;
+int Solution::paint(int A, int B, vector<int> &C) {
+    int n = C.size();
+    long lo = INT_MIN;
+    long hi = 0;
+
+    vector<long> time(n);
+
+    for(int i = 0; i < n; i++) {
+        time[i] = (long)C[i] * (long)B;
+        lo = max(lo, time[i]);
+        hi += time[i];
     }
-	return 0;
+
+    long ans = -1;
+    while(lo <= hi) {
+        long mid = hi - (hi-lo) / 2;
+        if(check(time, A, mid)) {
+            ans = mid;
+            hi = mid - 1;
+        }
+        else lo = mid + 1;
+    }
+    return ans % 10000003;
 }
