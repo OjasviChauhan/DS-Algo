20f8b0911b381b5d44094f509f2d0931550ef321
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-08-25 14:57:24 +0530

    Update [BST] Validate Binary Search Tree.cpp

--- Binary Trees/[BST] Validate Binary Search Tree.cpp
+++ Binary Trees/[BST] Validate Binary Search Tree.cpp
@@ -2,6 +2,29 @@
 
 class Solution {
 public:
+    bool isValidBST(TreeNode* root) {
+        if(!root) return true;
+        TreeNode* mn = NULL;
+        stack<TreeNode*> s;
+        
+        while(root or !s.empty()) {
+            while(root){
+                s.push(root);
+                root = root->left;
+            }
+            root = s.top(); 
+            s.pop();
+            if(mn != NULL and root->val <= mn->val) return false;
+            mn = root;
+            root = root->right;
+        }
+        return true;
+    }
+};
+
+// SOLUTION 2
+class Solution {
+public:
      bool isValidBST(TreeNode* root, TreeNode* min=NULL, TreeNode* max=NULL) {
         if (!root) 
             return true;
