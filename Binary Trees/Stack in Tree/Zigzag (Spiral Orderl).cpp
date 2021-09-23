089d443b415d6264508d14515ed66a5a0f29eb09
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-09-23 17:28:31 +0530

    Update Zigzag (Spiral Orderl).cpp

--- Binary Trees/Stack in Tree/Zigzag (Spiral Orderl).cpp
+++ Binary Trees/Stack in Tree/Zigzag (Spiral Orderl).cpp
@@ -1,14 +1,46 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+
+// Solution 1
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        
+        if(!root) return ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        bool leftToRight = true;
+        
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> ds(size);
+            
+            for(int i = 0; i < size; i++){
+                auto t = q.front();
+                q.pop();
+                
+                int idx = leftToRight ? i : size-i-1;
+                ds[idx] = t->val;
+                
+                if(t->left) {
+                    q.push(t->left);
+                }
+                if(t->right){
+                    q.push(t->right);
+                }
+            }
+            leftToRight = !leftToRight;
+            ans.push_back(ds);
+        }
+        return ans;
+    }
+};
+
+
+// Solution 2
+
 class Solution {
 public:
     void zigZag(TreeNode* root, vector<vector<int>> &v){
