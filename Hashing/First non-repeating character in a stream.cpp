a0dc4cbe774c15542c44ecff6948957ac0510076
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-09-23 10:47:24 +0530

    Update First non-repeating character in a stream.cpp

--- Hashing/First non-repeating character in a stream.cpp
+++ Hashing/First non-repeating character in a stream.cpp
@@ -1,22 +1,22 @@
 // https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1#
 
-string FirstNonRepeating(string A){
-	unordered_set<char> s;
-	string ans = "";
+string FirstNonRepeating(string A) {
+    unordered_set<char> s;
+    string ans = "";
     list<char> q;
-            
-    for(char &c : A) {
-    	if(s.find(c) == s.end()){
+
+    for (char &c : A) {
+        if (s.find(c) == s.end()) {
             q.push_back(c);
-        	s.insert(c);
+            s.insert(c);
         }
-        else if(!q.empty() and c == q.front()){
-        	q.pop_front();
+        else if (!q.empty() and c == q.front()) {
+            q.pop_front();
         }
-        else{
-        	q.remove(c);
+        else {
+            q.remove(c);
         }
         ans += !q.empty() ? q.front() : '#';
-	}
+    }
     return ans;
 }
