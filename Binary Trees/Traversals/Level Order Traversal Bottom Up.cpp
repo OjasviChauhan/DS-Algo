174470c54d4cf1370305190d5f9fd58add7194b3
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2021-09-23 23:09:38 +0530

    Update Level Order Traversal Bottom Up.cpp

--- Binary Trees/Traversals/Level Order Traversal Bottom Up.cpp
+++ Binary Trees/Traversals/Level Order Traversal Bottom Up.cpp
@@ -1,14 +1,6 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// https://leetcode.com/problems/binary-tree-level-order-traversal-ii/
+
+// SOLUTION 1
 class Solution {
 public:
     void bfsReverse(TreeNode* root, vector<vector<int>> &v){
@@ -64,3 +56,36 @@ public:
         return v;
     }
 };
+
+// SOLUTION 2
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> ds;
+            for(int i = 0; i < size; i++) {
+                auto t = q.front();
+                q.pop();
+                
+                ds.push_back(t->val);
+                
+                if(t->left){
+                    q.push(t->left);
+                }
+                if(t->right){
+                    q.push(t->right);
+                }
+            }
+            ans.push_back(ds);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
