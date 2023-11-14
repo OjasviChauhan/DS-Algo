6a410fd8428f2cf7624abf30d0a8770f716b92ef
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2023-11-14 21:26:34 +0530

    Update Next Permutation.cpp

--- Arrays/Imp Manipulations (Space Optimizations)/Next Permutation.cpp
+++ Arrays/Imp Manipulations (Space Optimizations)/Next Permutation.cpp
@@ -20,23 +20,22 @@ https://www.youtube.com/watch?v=LuLCLgMElus&feature=emb_logo
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-    	int n = nums.size(), k, l;
-    	for(k = n-2; k>=0; k--){
-            if(nums[k] < nums[k+1]){
+        int i,j; 
+        for(i = nums.size()-2; i >= 0; i--){
+            if(nums[i] < nums[i+1]) {
                 break;
+            }     
+        }
+        if(i == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        for(j = nums.size()-1; j > i; j--){
+            if(nums[j] > nums[i]){
+                swap(nums[i], nums[j]);
+                reverse(nums.begin() + i + 1, nums.end());
+                return;
             }
         }
-    	if(k < 0){
-    	    reverse(nums.begin(), nums.end());
-    	} 
-        else{
-    	    for(l = n-1; l>k; l--){
-                if(nums[l] > nums[k]){
-                    break;
-                }
-            } 
-    	    swap(nums[k], nums[l]);
-    	    reverse(nums.begin() + k + 1, nums.end());
-        }
     }
-}; 
+};
