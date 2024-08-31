1ea80c0746b0e514e267339815b2117ec04c5968
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2024-08-31 17:24:08 +0530

    Update Sort an array of 0s, 1s and 2s.cpp

--- Arrays/Imp Manipulations (Space Optimizations)/Sort an array of 0s, 1s and 2s.cpp
+++ Arrays/Imp Manipulations (Space Optimizations)/Sort an array of 0s, 1s and 2s.cpp
@@ -1,5 +1,20 @@
 // https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1#
 
+/*
+This problem is the same as the famous “Dutch National Flag problem”.
+
+    "Given N balls of colour red, white or blue arranged in a line in random order. 
+    You have to arrange all the balls such that the balls with the same colours are 
+    adjacent with the order of the balls, with the order of the colours being red, 
+    white and blue (i.e., all red coloured balls come first then the white coloured
+    balls and then the blue coloured balls)."
+
+    arr[0] to arr[lo – 1]: This part will have all the 0s.
+    arr[lo] to arr[mid – 1]: This part will have all the 1s.
+    arr[mid] to arr[hi]: This will be our unsorted array.
+    arr[hi + 1] to arr[N – 1]: This part will have all the 2s.
+*/
+
 class Solution
 {
     public:
