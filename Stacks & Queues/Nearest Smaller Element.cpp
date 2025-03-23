96cfb47027bd1c2b467acce4f2688a3a6e2ffd73
Ojasvi Chauhan <ojasvichauhan2304@gmail.com>
2025-03-23 21:35:33 +0530

    Update Nearest Smaller Element.cpp

--- Stacks & Queues/Nearest Smaller Element.cpp
+++ Stacks & Queues/Nearest Smaller Element.cpp
@@ -1,23 +1,13 @@
 vector<int> Solution::prevSmaller(vector<int> &A) {
-    vector<int> v;
-    stack<int> s;
-    for(int i=0;i<A.size();i++){
-        if(s.empty()){
-            v.push_back(-1);
-        }
-        else if(!s.empty() && A[i]>s.top()){
-            v.push_back(s.top());
-        }
-        else if(!s.empty() && A[i]<=s.top()){
-            while(!s.empty() && s.top()>=A[i]){
-                s.pop();
-            }
-            if(s.empty())
-                v.push_back(-1);
-            else
-                v.push_back(s.top());
-        }
-        s.push(A[i]);
+    stack<int> st;
+    vector<int> ans(A.size());
+    
+    for(int i=0; i<A.size(); i++) {
+        while(!st.empty() && st.top() >= A[i]) 
+            st.pop();
+        ans[i] = st.empty() ? -1 : st.top();
+        st.push(A[i]);
     }
-    return v;
+    
+    return ans;
 }
